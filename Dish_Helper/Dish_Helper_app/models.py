867cd8bd8e82297d2d139666a7e8610585ae5da7
pailime <emilia.palczyk@gmail.com>
2023-03-13 21:24:56 +0100

    Initial commit

--- Dish_Helper/Dish_Helper_app/models.py
+++ Dish_Helper/Dish_Helper_app/models.py
@@ -8,3 +8,12 @@ class Breakfast(models.Model):
     preparation_time = models.PositiveIntegerField()
     portions = models.PositiveSmallIntegerField()
 
+
+class Dinner(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField()
+    preparation = models.TextField()
+    preparation_time = models.PositiveIntegerField()
+    portions = models.PositiveSmallIntegerField()
+
+
