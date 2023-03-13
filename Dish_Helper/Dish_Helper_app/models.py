69b96c4e3ae3a372ed3e9c68827dc8ef18bb4370
pailime <emilia.palczyk@gmail.com>
2023-03-13 21:19:13 +0100

    Initial commit

--- Dish_Helper/Dish_Helper_app/models.py
+++ Dish_Helper/Dish_Helper_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Breakfast(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField()
+    preparation = models.TextField()
+    preparation_time = models.PositiveIntegerField()
+    portions = models.PositiveSmallIntegerField()
+
