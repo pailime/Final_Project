a6e22527c7f9a068a754bd38470660f7d815eb96
pailime <emilia.palczyk@gmail.com>
2023-03-13 21:55:57 +0100

    Initial commit

--- Dish_Helper/Dish_Helper_app/models.py
+++ Dish_Helper/Dish_Helper_app/models.py
@@ -4,10 +4,16 @@ from django.db import models
 class Meal(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField()
-    preparation = models.TextField()
-    preparation_time = models.PositiveIntegerField()
-    portions = models.PositiveSmallIntegerField()
+    recipe = models.TextField()
+    total_time = models.PositiveIntegerField()
+    servings = models.PositiveSmallIntegerField()
+    ingredients = models.ManyToManyField('Ingredient')
 
 
-class Ingredients(models.Model):
+class Ingredient(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    calories = models.PositiveSmallIntegerField()
+    fat = models.PositiveSmallIntegerField()
+    carbs = models.PositiveSmallIntegerField()
+    protein = models.PositiveSmallIntegerField()
+
