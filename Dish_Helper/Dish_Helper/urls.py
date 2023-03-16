c8bef8e8b7d1903ca5de6ad3fd4fb8bdb020f3a5
pailime <emilia.palczyk@gmail.com>
2023-03-16 21:27:21 +0100

    Initial commit

--- Dish_Helper/Dish_Helper/urls.py
+++ Dish_Helper/Dish_Helper/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from Dish_Helper_app.view import MainPageView
+from Dish_Helper_app.views import MainPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
