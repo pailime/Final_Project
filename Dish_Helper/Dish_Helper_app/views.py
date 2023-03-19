e9fe8bba39a9f47f52779bc606a954a72f483aaa
pailime <emilia.palczyk@gmail.com>
2023-03-19 18:18:29 +0100

    Initial commit

--- Dish_Helper/Dish_Helper_app/views.py
+++ Dish_Helper/Dish_Helper_app/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.template import loader
 from django.views import View
 
 from .models import User, Meal, TypeOfMeal, Ingredient, IngredientMeasurement
@@ -6,3 +8,11 @@ from .models import User, Meal, TypeOfMeal, Ingredient, IngredientMeasurement
 
 class MainPageView(View):
     pass
+
+
+class AddMealView(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
