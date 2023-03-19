6bae5fc6f88cf446d9c3c0b219f48c244362ead8
pailime <emilia.palczyk@gmail.com>
2023-03-19 15:51:57 +0100

    Initial commit

--- Dish_Helper/Dish_Helper_app/models.py
+++ Dish_Helper/Dish_Helper_app/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    user_name = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.user_name
 
 
 class Meal(models.Model):
