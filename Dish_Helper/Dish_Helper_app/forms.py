152485e24ce309807b7ea89888a51bf8f2be440b
pailime <emilia.palczyk@gmail.com>
2023-03-28 21:40:29 +0200

    Initial commit

--- Dish_Helper/Dish_Helper_app/forms.py
+++ Dish_Helper/Dish_Helper_app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-from .models import Profile
+from .models import Profile, Ingredient
 
 
 class UserRegisterForm(UserCreationForm):
@@ -13,3 +13,16 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+
+class MealForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=128)
+    description = forms.CharField(label='Description:', widget=forms.Textarea)
+    recipe = forms.CharField(label='Recipe:', widget=forms.Textarea)
+    total_time = forms.IntegerField(label='Total preparation time in seconds', min_value=0, max_value=360)
+    servings = forms.IntegerField(label='Number od servings')
+    measurement = forms.ModelMultipleChoiceField(
+        label='Ingedients:',
+        queryset=Ingredient.objects.all(),
+        widget=forms.CheckboxSelectMultiple
+    )
