256e218bb9be5370881da948a8c56109790e64d0
pailime <emilia.palczyk@gmail.com>
2023-04-03 23:08:20 +0200

    Initial commit

--- Dish_Helper/Dish_Helper_app/views.py
+++ Dish_Helper/Dish_Helper_app/views.py
@@ -1,8 +1,9 @@
 import random
 from urllib import request
 
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import CreateView, FormView
@@ -16,8 +17,7 @@ class MainPageView(View):
     def get(self, request):
         meals = list(Meal.objects.all())
         ingredients = Ingredient.objects.all()
-        for m in meals:
-            random.shuffle(meals)
+        random.shuffle(meals)
         meal1 = meals[0]
         return render(
             request,
@@ -42,18 +42,19 @@ class MealDetailView(View):
         meal_id = int(id)
         meal = Meal.objects.get(id=meal_id)
         context = {'meal': meal}
-        return render(request, 'mealdetail.html', context)
+        return render(request, 'templates/Dish_Helper_app/meal_detail.html', context)
 
 
 # Profile user view
 class ProfileLoginView(FormView):
-    template_name = 'profile_form.html'
+    template_name = 'templates/Dish_Helper_app/profile_form.html'
     success_url = 'base'
 
     def form_valid(self, form):
         user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
         if user is not None:
             login(self, request, user)
+            messages.success(self.request, "You've been logged in")
         return super().form_valid(form)
 
 
@@ -63,14 +64,18 @@ class ProfileRegisterView(View):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Your account has been created! You are now able to log in.')
             return redirect('base')
         else:
+            error_messages = list(form.errors.values())
+            for message in error_messages:
+                messages.error(request, message)
             return redirect('register')
 
     def get(self, request):
         form = UserRegisterForm()
         context = {'form': form}
-        return render(request, 'register.html', context)
+        return render(request, 'templates/Dish_Helper_app/register.html', context)
 
 
 # View where authenticated user can add meal
@@ -79,12 +84,22 @@ class AddMealView(CreateView):
     fields = ['name', 'description', 'recipe', 'total_time', 'servings', 'measurement']
     success_url = reverse_lazy('base')
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Meal was added successfully.')
+        return response
+
 
 # View where authenticated user can add type of meal
 class AddTypeOfMealView(CreateView):
     model = TypeOfMeal
     fields = ['type_of_meal', 'meal']
     success_url = reverse_lazy('base')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Type of meal was added successfully.')
+        return response
 
 
 # View where authenticated user can add ingredient
@@ -93,9 +108,19 @@ class AddIngredientView(CreateView):
     fields = ['name', 'calories', 'fat', 'carbs', 'protein']
     success_url = reverse_lazy('base')
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Ingredient was added successfully.')
+        return response
+
 
 # View where authenticated user can add ingredient measurement
 class AddIngredientMeasurementView(CreateView):
     model = IngredientMeasurement
     fields = ['weight', 'ingredient_id', 'meal_id']
     success_url = reverse_lazy('base')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Ingredient Measurement was added successfully.')
+        return response
