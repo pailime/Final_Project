f0d8b1b546aea5757c2f510da15304981f24867a
pailime <emilia.palczyk@gmail.com>
2023-04-07 22:38:53 +0200

    Initial commit

--- Dish_Helper/Dish_Helper_app/tests.py
+++ Dish_Helper/Dish_Helper_app/tests.py
@@ -1,4 +1,5 @@
 import pytest
+from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.test import Client
@@ -29,4 +30,59 @@ def test_login(client, user_data, user):
     assert response.url == reverse('base')
     assert client.login(**user_data) is True
 
+
+@pytest.mark.django_db
+def test_set_check_password(user):
+    # user.set_password("new-password")
+    assert user.check_password('testpass') is True
+    assert user.check_password('testpass1') is False
+
+
+def test_set_check_user(user):
+    assert user.username == 'testuser'
+
+
+@pytest.fixture
+def new_user_one(db):
+    def create_app_user(
+            username: str,
+            password: str = None,
+            first_name: str = 'firstname',
+            last_name: str = 'lastname',
+            email: str = 'test@test.com',
+            is_superuser: str = False,
+            is_active: str = True,
+    ):
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            is_superuser=is_superuser,
+            is_active=is_active
+        )
+        return user
+    return create_app_user
+
+
+@pytest.fixture
+def user_one(db, new_user_one):
+    return new_user_one('testuser', 'password', 'somename', 'somelast', 'whatever@test.com', is_superuser='False', is_active='True')
+
+
+def test_new_user(user_one):
+    assert user_one.username == 'testuser'
+    # assert user_one.password == 'password'
+    assert user_one.first_name == 'somename'
+    assert user_one.last_name == 'somelast'
+    assert user_one.email == 'whatever@test.com'
+    assert user_one.is_superuser
+    assert user_one.is_active
+
+
+class AddMealViewTest(TestCase):
+    def test_view_url_at_desired_location(self):
+        response = self.client.get('/add_meal/')
+        self.assertEqual(response.status_code, 200)
 
