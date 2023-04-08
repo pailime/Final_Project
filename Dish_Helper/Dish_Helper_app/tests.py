a448abaaf6d82e367ec42c0f82c1d6a04e8facab
pailime <emilia.palczyk@gmail.com>
2023-04-08 20:55:11 +0200

    Initial commit

--- Dish_Helper/Dish_Helper_app/tests.py
+++ Dish_Helper/Dish_Helper_app/tests.py
@@ -6,24 +6,7 @@ from django.test import Client
 from django.http import HttpResponse
 
 
-@pytest.fixture
-def client():
-    return Client()
-
-
-@pytest.fixture
-def user_data():
-    return {
-        'username': 'testuser',
-        'password': 'testpass',
-    }
-
-
-@pytest.fixture
-def user(db, user_data):
-    return User.objects.create_user(**user_data)
-
-
+@pytest.mark.django_db
 def test_login(client, user_data, user):
     url = reverse('login')
     response = client.post(url, data=user_data)
@@ -38,39 +21,12 @@ def test_set_check_password(user):
     assert user.check_password('testpass1') is False
 
 
+@pytest.mark.django_db
 def test_set_check_user(user):
     assert user.username == 'testuser'
 
 
-@pytest.fixture
-def new_user_one(db):
-    def create_app_user(
-            username: str,
-            password: str = None,
-            first_name: str = 'firstname',
-            last_name: str = 'lastname',
-            email: str = 'test@test.com',
-            is_superuser: str = False,
-            is_active: str = True,
-    ):
-        user = User.objects.create_user(
-            username=username,
-            password=password,
-            first_name=first_name,
-            last_name=last_name,
-            email=email,
-            is_superuser=is_superuser,
-            is_active=is_active
-        )
-        return user
-    return create_app_user
-
-
-@pytest.fixture
-def user_one(db, new_user_one):
-    return new_user_one('testuser', 'password', 'somename', 'somelast', 'whatever@test.com', is_superuser='False', is_active='True')
-
-
+@pytest.mark.django_db
 def test_new_user(user_one):
     assert user_one.username == 'testuser'
     assert user_one.check_password('password')
@@ -84,44 +40,24 @@ def test_new_user(user_one):
 @pytest.mark.django_db
 def test_add_meal(client, user_data):
     response = client.get('/add_meal/')
-    assert response.status_code == 200
+    assert response.status_code == 302
     response2 = client.get('')
     assert response2.status_code == 200
-
-# @pytest.mark.django_db
-# def test_add_meal_reverse(client):
-#     response2 = client.get(reverse('base'))
-#     assert response.status_code == 200
 
 
 @pytest.mark.django_db
 def test_add_type_url(client):
     response = client.get('/add_type/')
-    assert response.status_code == 200
-
-@pytest.mark.django_db
-def test_add_type_reverse(client):
-    response = client.get(reverse('base'))
-    assert response.status_code == 200
+    assert response.status_code == 302
 
 
 @pytest.mark.django_db
 def test_add_ingredient_url(client):
     response = client.get('/add_ingredient/')
-    assert response.status_code == 200
-
-@pytest.mark.django_db
-def test_add_ingredient_reverse(client):
-    response = client.get(reverse('base'))
-    assert response.status_code == 200
+    assert response.status_code == 302
 
 
 @pytest.mark.django_db
 def test_add_measurement_url(client):
     response = client.get('/add_measurement/')
-    assert response.status_code == 200
-
-@pytest.mark.django_db
-def test_add_measurement_reverse(client):
-    response = client.get(reverse('base'))
-    assert response.status_code == 200
+    assert response.status_code == 302
