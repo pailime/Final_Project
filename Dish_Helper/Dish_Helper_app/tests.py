f5bfe5e08a6f2f2decd038a8e902eec42e01d03b
pailime <emilia.palczyk@gmail.com>
2023-04-09 23:24:16 +0200

    Initial commit

--- Dish_Helper/Dish_Helper_app/tests.py
+++ Dish_Helper/Dish_Helper_app/tests.py
@@ -1,14 +1,18 @@
 import pytest
 from django.urls import reverse
-from django.contrib.auth.models import User
-from django.test import Client
-from django.http import HttpResponse
-from django.contrib.messages import get_messages
 from Dish_Helper_app.models import Meal, TypeOfMeal, Ingredient, IngredientMeasurement
+from pytest_django.asserts import assertTemplateUsed
 
 
 @pytest.mark.django_db
 def test_login(client, user_data, user):
+    """
+    This test checks the login functionality of the Django application.
+    :param client:
+    :param user_data:
+    :param user:
+    :return:
+    """
     url = reverse('login')
     response = client.post(url, data=user_data)
     assert response.status_code == 302
@@ -45,67 +49,83 @@ def test_main_page(client, user):
 
 
 @pytest.mark.django_db
-def test_meal_detail(client, user, meal):
+def test_meal_detail(client, user, meal, user_data):
     response = client.get(f'/mealdetail/{meal.id}/')
     assert response.status_code == 302
-    assert response.url == (f'/accounts/login/?next=/mealdetail/{meal.id}/')
+    assert response.url == f'/accounts/login/?next=/mealdetail/{meal.id}/'
+    response3 = client.get(f'/mealdetail/{not meal.id}/')
+    assert response3.status_code == 404
     client.force_login(user)
     response1 = client.get(f'/mealdetail/{meal.id}/')
     assert response1.status_code == 200
+    assert client.login(**user_data) is True
+    response2 = client.get(f'/mealdetail/{not meal.id}/')
+    assert response2.status_code == 404
+
 
 
 @pytest.mark.django_db
-def test_add_meal(client, user, meal):
+def test_add_meal(client, user, meal, user_data):
     response = client.get('/add_meal/')
     assert response.status_code == 302
-    assert response.url == ('/login/?next=/add_meal/')
+    assert response.url == '/login/?next=/add_meal/'
     client.force_login(user)
     response1 = client.get('/add_meal/')
     assert response1.status_code == 200
+    assert client.login(**user_data) is True
     response2 = client.post('/add_meal/', meal=meal)
     assert response2.status_code == 200
     assert Meal.objects.get(name=meal.name)
     print(meal.name)
     assert meal is not None
+    assertTemplateUsed(response2, 'templates/Dish_Helper_app/meal_form.html')
 
 
 @pytest.mark.django_db
-def test_add_type(client, user, type_of_meal):
+def test_add_type(client, user, type_of_meal, user_data):
     response = client.get('/add_type/')
     assert response.status_code == 302
-    assert response.url == ('/login/?next=/add_type/')
+    assert response.url == '/login/?next=/add_type/'
     client.force_login(user)
     response1 = client.get('/add_type/')
     assert response1.status_code == 200
+    assert client.login(**user_data) is True
     response2 = client.post('/add_type/', type_of_meal=type_of_meal)
     assert response2.status_code == 200
     assert TypeOfMeal.objects.get(id=type_of_meal.id)
+    print(type_of_meal.type_of_meal)
     assert type_of_meal is not None
+    assertTemplateUsed(response2, 'templates/Dish_Helper_app/typeofmeal_form.html')
 
 
 @pytest.mark.django_db
-def test_add_ingredient_url(client, user, ingredient):
+def test_add_ingredient(client, user, ingredient, user_data):
     response = client.get('/add_ingredient/')
     assert response.status_code == 302
-    assert response.url == ('/login/?next=/add_ingredient/')
+    assert response.url == '/login/?next=/add_ingredient/'
     client.force_login(user)
     response1 = client.get('/add_ingredient/')
     assert response1.status_code == 200
+    assert client.login(**user_data) is True
     response2 = client.post('/add_ingredient/', ingredient=ingredient)
     assert response2.status_code == 200
     assert Ingredient.objects.get(name=ingredient.name)
+    print(ingredient.name)
     assert ingredient is not None
+    assertTemplateUsed(response2, 'templates/Dish_Helper_app/ingredient_form.html')
 
 
 @pytest.mark.django_db
-def test_add_measurement_url(client, user, ingredientmeasurement):
+def test_add_measurement(client, user, ingredientmeasurement):
     response = client.get('/add_measurement/')
     assert response.status_code == 302
-    assert response.url == ('/login/?next=/add_measurement/')
+    assert response.url == '/login/?next=/add_measurement/'
     client.force_login(user)
     response1 = client.get('/add_measurement/')
     assert response1.status_code == 200
     response2 = client.post('/add_measurement/', ingredientmeasurement=ingredientmeasurement)
     assert response2.status_code == 200
     assert IngredientMeasurement.objects.get(id=ingredientmeasurement.id)
+    print(ingredientmeasurement.id)
     assert ingredientmeasurement is not None
+    assertTemplateUsed(response2, 'templates/Dish_Helper_app/ingredientmeasurement_form.html')
